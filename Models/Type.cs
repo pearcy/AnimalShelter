8f5cffc9705dc09a436d148cd11da6e64d52662a
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-28 15:37:21 -0700

    add create view
    co-authored-by: Teresa Rosinski <Trosinski89@gmail.com>
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- Models/Type.cs
+++ Models/Type.cs
@@ -3,6 +3,15 @@ namespace AnimalShelter.Models
   public class Type
   {
     public int TypeId { get; set; }
+
+    public string Gender {get; set;}
+
+    public string DateOfAdmittance {get; set;}
+
+    public string Breed {get; set;}; 
+
+    public string Name {get; set;}
+
     public string Description { get; set; }
   }
 }
