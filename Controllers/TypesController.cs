0a4b8a67d2087f19f95bbc6ebad8e33ed03a4208
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-28 14:50:17 -0700

    add types controller
    co-authored-by: Teresa Rosinski <Trosinski89@gmail.com>
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- Controllers/TypesController.cs
+++ Controllers/TypesController.cs
@@ -1,1 +1,45 @@
-name
+using Microsoft.AspNetCore.Mvc; 
+using AnimalShelter.Models; 
+using System.Collections.Generic; 
+using System.Linq; 
+
+namespace AnimalShelter.Controllers
+{
+  public class TypeController : Controller
+  {
+    private readonly AnimalShelterContext _db; // declare priave readonly AnimalShelterContext
+
+    public TypesController(AnimalShelterContext db) // constructor that sets the value of _db proprty to TypesContext
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Type> model = _db.Types.ToList(); //replaces GetAll() // ToLinst () =LINQ method 
+      return View(model);
+      //db = isntance of DbContext class - it holds reference to database
+      //LINQ turns DbSet into a list using ToList() (came from System.Linq)
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Type type)
+    {
+      _db.Types.Add(type);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id) //takes id of entry we want to view --> must match property from anonymous object created using ActionLink() in view
+    {
+      Type thisType = _db.Types.FirstOrDefault(types => types.TypeId == id);
+      return View(thisType);
+      //FirstOrDefault = Linq method which uses a lambda -- look at db.Types (types table) - find any types where the type id matches the id being provided to this method
+    }
+  }
+}  
